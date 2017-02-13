c03ebd6d14903ab1d348f8dbc1a32da94e6cb290
Aubrey Badger <akbadger@gmail.com>
2017-02-13 15:21:39 -0500

    created madlib homework

---
+++ madlib/js/madlib.js
@@ -0,0 +1,48 @@
+var noun1 = 'water';
+var number1 = '28';
+var noun2 = 'duck';
+var noun3 = 'backpack';
+var unitOfTime1 = 'millisecond';
+var unitOfTime2 = 'week';
+var verbPresent = 'floating';
+var adjective1 = 'frightening';
+var verb1 = 'hop';
+var adjective2 = 'black';
+var noun4 = 'leaf';
+var adjective3 = 'striped';
+var noun5 = 'Detroit';
+var verb2 = 'drive';
+var number2 = '200';
+var number3 = '52';
+var unitOfMeasure1 = 'centimeter';
+var adjective4 = 'massive';
+var adjective5 = 'flat';
+var adjective6 = 'bumpy';
+
+
+
+var story = `The two ${noun1} and the small boy stood absolutely still on the grass underneath the tree, gazing up at this extraordinary ${noun2}. James's little face was glowing with excitement, his eyes were as big and bright as ${number1} ${noun3}. He could see the ${noun2} swelling larger and larger as clearly as if it were a balloon being blown up. In half a ${unitOfTime1}, it was the size of a melon! In another half-${unitOfTime2}, it was twice as big again! 
+"Just look at it ${verbPresent}!" Aunt Spiker cried.
+"Will it ever stop!" Aunt Sponge shouted, waving her ${adjective1}
+arms and starting to ${verb1} around in circles.
+And now it was so ${adjective2} it looked like an enormous buttercolored
+${noun4} dangling from the top of the tree.
+"Get away from that tree trunk, you ${adjective3} ${noun5}!" Aunt
+Spiker yelled. "The slightest shake and I'm sure it'll ${verb2} off! It
+must weigh ${number2} or ${number3} ${unitOfMeasure1} at least!"
+The branch that the ${noun2} was ${verbPresent} upon was
+beginning to bend over further and further because of the
+weight.
+"Standback!" Aunt Sponge shouted. "It's coming down!
+The branch is going to break!"
+But the branch didn't break. It simply bent over more and
+more as the ${noun2} got heavier and heavier.
+And still it went on ${verbPresent}.
+In another minute, this mammoth ${noun2} was as ${adjective4} and
+${adjective5} and ${adjective6} as Aunt Sponge herself, and probably just as
+heavy.
+"It has to stop now!" Aunt Spiker yelled. "It can't go on
+forever!"
+But it didn't stop.`
+
+console.log(story);
